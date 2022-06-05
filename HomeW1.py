a6439775abc24118891e4e139093a7e9d9dc821c
Yaroslav.S <sysoev.yar@yandex.ru>
2022-06-05 18:08:31 +0500

    Решил задачу 1

--- HomeW1.py
+++ HomeW1.py
@@ -1,3 +1,11 @@
+# Сформировать список из N членов последовательности
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
+N = int(input("Введи число N = "))
+array = range(N)
 
-print("hello world!")
+for i in array:
+    if i % 2:
+        print(-3 ** i)
+    else:
+        print(3 ** i)
